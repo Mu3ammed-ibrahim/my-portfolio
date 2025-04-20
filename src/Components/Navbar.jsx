f0a5deb6390a6745e36d9c73323d75401f219c6c
Muhammed Almutasim Ibrahim <www.tota.11@gmail.com>
2025-04-20 04:46:32 +0300

    enhanced my navbar styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
+
 const Navbar = () => {
   return (
     <motion.nav
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       transition={{ duration: 1 }}
-      className="py-6 px-8 flex items-center sticky top-0 z-10 justify-between"
+      className="py-6 px-8 flex items-center sticky top-0 z-10 justify-between bg-zinc-900 bg-opacity-10 backdrop-filter backdrop-blur-lg  shadow-lg"
     >
       <h2 className="text-green-700 text-3xl font-bold">{`</Mo>`}</h2>
       <div className="flex space-x-8">
