b71607bdac01847810b90bb2e6351c87fcba9316
Muhammed Almutasim Ibrahim <www.tota.11@gmail.com>
2026-04-17 23:19:02 +0300

    chore: remove unused React default import from StickyScroll

--- src/Components/ui/sticky-scroll-reveal.jsx
+++ src/Components/ui/sticky-scroll-reveal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "motion/react";
 import { useInView } from "react-intersection-observer";
 import { cn } from "@/lib/utils";
